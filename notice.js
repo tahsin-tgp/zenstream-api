9cd67719fa2c217b1551842126d2f5ce77dd5273
Tahsinul Haque <rubelart881@gmail.com>
2026-07-12 01:50:02 +0600

    Update notice.js

--- notice.js
+++ notice.js
@@ -1,5 +1,5 @@
 var noticeData = {
-  "show_notice": true,
+  "show_notice": false,
   "title": "🚨 গুরুত্বপূর্ণ ঘোষণা",
   "message": "Zen Stream App এখনও Development Phase-এ রয়েছে। তাই অ্যাপের কিছু সেকশন এখনো সম্পূর্ণভাবে প্রস্তুত নয় এবং সেগুলো আপাতত কাজ নাও করতে পারে।\n\n✅ তবে চিন্তার কোনো কারণ নেই!\n⚽ FIFA World Cup Live Streaming সম্পূর্ণ প্রস্তুত এবং কোনো সমস্যা ছাড়াই উপভোগ করতে পারবেন।\n\n🏏 Cricket Channel ও অন্যান্য নতুন ফিচার খুব শীঘ্রই যোগ করা হবে। আমরা প্রতিনিয়ত অ্যাপটি আরও উন্নত করছি।",
   "image_url": "",
