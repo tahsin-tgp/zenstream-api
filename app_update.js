c0a3eea24a3b9ea25a86c634f061b88af781e9b3
Tahsinul Haque <rubelart881@gmail.com>
2026-07-20 20:47:10 +0600

    Update app_update.js

--- app_update.js
+++ app_update.js
@@ -1,6 +1,6 @@
 {
-  "latest_version": "1.1.0",
-  "force_update": false,
-  "update_url": "https://zenstreamm.pages.dev/downloads/zenstream_v1.1.0.apk",
-  "change_log": "১. ডাউনলোড স্পিড এবং Time Remaining ডিসপ্লে স্মুথ করা হয়েছে।\n২. ডাউনলোড সম্পন্ন হলে Back বাটন যোগ করা হয়েছে।\n৩. বারবার ক্লিক প্রতিরোধ করার ব্যবস্থা যোগ করা হয়েছে।\n৪. ইতিমধ্যে ডাউনলোড করা থাকলে 'Play Offline' বাটন দেখাবে।\n৫. YouTube প্লেব্যাক 15 সেকেন্ড টাইমআউট সহ উন্নত করা হয়েছে।\n৬. ভাষা সতর্কতা শুধুমাত্র বাংলায় দেখানো হচ্ছে।"
+  "latest_version": "1.2.1",
+  "force_update": true,
+  "update_url": "https://www.googleapis.com/drive/v3/files/1VGm1QWPW8yC8-xC4e_UngIWWj5FqgTMC?alt=media&key=AIzaSyCZ44iNZB_GORCFFPpkapzf2neEyFXc6ZU",
+  "change_log": "১. সার্ভার লোডিং স্পিড বৃদ্ধি করা হয়েছে।\n২. নতুন ভিডিও প্লেয়ারে PiP ও স্ক্রিন লক যুক্ত করা হয়েছে।"
 }
